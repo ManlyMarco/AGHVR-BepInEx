82d6e20ad12390a9287e32dc455a16c2d47a82c7
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-08-25 00:35:05 +0200

    Convert to BepInEx

--- AGHVR/AGHVR.cs
+++ AGHVR/AGHVR.cs
@@ -1,48 +1,17 @@
-﻿using IllusionPlugin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using UnityEngine;
+﻿using System;
+using BepInEx;
 using VRGIN.Core;
-using VRGIN.Helpers;
-using VRGIN.Modes;
 
 namespace AGHVR
 {
-    public class AGHVR : IEnhancedPlugin
+    [BepInPlugin(GUID, GUID, Version)]
+    public class AGHVR : BaseUnityPlugin
     {
-        public string[] Filter
-        {
-            get
-            {
-                return new string[] { "AGH" };
-            }
-        }
+        public const string GUID = "AGHVR";
 
-        public string Name
-        {
-            get
-            {
-                return "AGHVR";
-            }
-        }
+        public const string Version = "0.1";
 
-        public string Version
-        {
-            get
-            {
-                return "0.0";
-            }
-        }
-
-        public void OnApplicationQuit()
-        {
-        }
-        
-        public void OnApplicationStart()
+        public void Start()
         {
             if (Environment.CommandLine.Contains("--vr"))
             {
@@ -52,27 +21,6 @@ namespace AGHVR
             }
             //VRLog.Info("Layers: " + string.Join(", ", UnityHelper.GetLayerNames(int.MaxValue)));
             //UnityEngine.SceneManagement.SceneManager.LoadScene(7);
-        }
-
-        public void OnFixedUpdate()
-        {
-        }
-
-        public void OnLateUpdate()
-        {
-        }
-
-        public void OnLevelWasInitialized(int level)
-        {
-            //VRLog.Info(string.Join(", ", GameObject.FindObjectsOfType<Renderer>().Select(r => r.name + ": "+ string.Join(" - ", r.sharedMaterials.Select(m => m.name).ToArray()) ).Distinct().ToArray()));
-        }
-
-        public void OnLevelWasLoaded(int level)
-        {
-        }
-
-        public void OnUpdate()
-        {
         }
     }
 }
